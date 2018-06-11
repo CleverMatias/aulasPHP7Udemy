1cb711848ffef8f901b9eba490a979b7bd86bac4
cleverson <clevermatias@hotmail.com>
2018-06-11 14:28:25 -0300

    Primeiro commit, criado index.php com links para arrays e operadores.php, criado estilo para o index com links em cardboard laterais

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>AulasPHP7Udemy</title>
+	<link rel="stylesheet" type="text/css" href="assets/estilo.css">
+</head>
+<body>
+	<div class="containerPrincipal">
+		
+	<div class="cartao"><p><a href="arrays.php">Arrays</a></p></div>
+
+	<div class="cartao"><p><a href="operadores.php">Operadores</a></p></div>
+
+	</div>
+</body>
+</html>
