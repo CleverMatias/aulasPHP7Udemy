6a3e824e82a351e719dd803769fe022223a5bb50
cleverson <clevermatias@hotmail.com>
2018-06-11 15:14:24 -0300

    adicionado conteúdo sobre arrays em arrays.php

--- arrays.php
+++ arrays.php
@@ -3,7 +3,33 @@
 <head>
 	<title>Arrays</title>
 </head>
-<body>
-	arrays
-</body>
+<body><pre>
+	<?php 
+		// Criando arrays
+		$frutas = array('Laranja', 'pera', 'limão');
+		$carros = ['monza', 'cobalt', 'gol'];
+
+		// Mostrando arrays
+		print_r($carros);
+
+		// Arrays multidimencionais 
+		$pessoas = array();
+
+		// Inserir dados em array multidimecionais
+		array_push($pessoas, array(
+				'nome'      => 'Cleverson',
+				'sobrenome' => 'Matias',
+				'sexo'		=> 'M',
+				'Profissão' => 'Programador PHP'
+			));
+
+		array_push($pessoas, array(
+				'nome'		=> 'Josiana',
+				'sobrenome' => 'Fernandes',
+				'sexo'		=> 'F',
+				'Profissão'	=> 'Vendedora'
+			));
+		print_r($pessoas)
+	 ?>
+</pre></body>
 </html>
